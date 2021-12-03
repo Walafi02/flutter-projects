a64e5c6bccb3d059523f900abca4a613560363f2
Walafi Ferreira <walafif81@gmail.com>
2021-12-03 08:23:09 -0300

    Implementando services

--- arquitetura/lib/app/app_controller.dart
+++ arquitetura/lib/app/app_controller.dart
@@ -1,16 +1,25 @@
+import 'package:arquitetura/app/interfaces/local_storage_interface.dart';
 import 'package:arquitetura/app/models/appconfig_model.dart';
+import 'package:arquitetura/app/services/shared_local_storage_service.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppController {
   static final AppController instance = AppController._();
 
-  AppController._();
+  AppController._() {
+    storage.get("isDark").then((value) => {
+          if (value != null) config.themeSwitch.value = value,
+        });
+  }
 
   final AppConfigModel config = AppConfigModel();
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
+  final ILocalStorage storage = SharedLocalStorageService();
+
   changeTheme(bool value) {
     config.themeSwitch.value = value;
+    storage.put("isDark", value);
   }
 }
---
+++ arquitetura/lib/app/services/shared_local_storage_service.dart
@@ -0,0 +1,32 @@
+import 'package:arquitetura/app/interfaces/local_storage_interface.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedLocalStorageService implements ILocalStorage {
+  @override
+  Future delete(String key) async {
+    SharedPreferences shared = await SharedPreferences.getInstance();
+
+    return shared.remove(key);
+  }
+
+  @override
+  Future get(String key) async {
+    SharedPreferences shared = await SharedPreferences.getInstance();
+
+    return shared.get(key);
+  }
+
+  @override
+  Future put(String key, value) async {
+    SharedPreferences shared = await SharedPreferences.getInstance();
+    if (value is bool) {
+      shared.setBool(key, value);
+    } else if (value is String) {
+      shared.setString(key, value);
+    } else if (value is int) {
+      shared.setInt(key, value);
+    } else if (value is double) {
+      shared.setDouble(key, value);
+    }
+  }
+}
